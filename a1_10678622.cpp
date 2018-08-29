a9e12273f5e8bc3cc1437b321f57c0c66e2338e3
balascoza1 <42818964+balascoza1@users.noreply.github.com>
2018-08-30 09:00:11 +1200

    Add files via upload

---
+++ a1_10678622.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main() {
+	int n, c=1;
+	cout<<"Please enter your number"<<endl;
+	cin>>n; i++) {
+		if (n%i !=0) {
+			continue;
+		}
+	else{
+		c=0;
+	}
+	if (c==0) {
+		cout<<"number is not prime"<<endl;
+	
+	}
+	else{
+		cout<<"number is a prime""<<endl;
+	}
+	return 0;
+}
